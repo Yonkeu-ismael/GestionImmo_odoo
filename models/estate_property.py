7b85792f75ac498004abbffb62ec727089d56506
ismael <ismaelyonkeu12@gmail.com>
2024-01-27 15:49:46 +0100

    Ajout des contrainte attributs et action aux forms et liste

--- models/estate_property.py
+++ models/estate_property.py
@@ -37,7 +37,7 @@ class Estate(models.Model):
         ('offer_accepted', 'Offre acceptée'),
         ('sold', 'Vendue'),
         ('cancelled', 'Annulée'),
-    ], string='Statut', default='new', readonly=True,required=True, copy=False)
+    ], string='Etat', default='new', readonly=True,required=True, copy=False)
     property_type_id = fields.Many2one("estate_property_type", string="Type propriété")
     salesman = fields.Many2one('res.users', string='Vendeur', index=True, default=lambda self: self.env.user.id)
     buyer = fields.Many2one('res.partner', string='Achéteur', index=True, default=lambda self: self.env.company.partner_id.id)
